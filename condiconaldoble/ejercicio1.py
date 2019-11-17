30b676a8fd4c9cd7e0ce5631fb3d02842434e0e5
982945464 <57601238+982945464@users.noreply.github.com>
2019-11-17 01:30:54 -0500

    Se creo el 1er ejecicio de condicional doble

---
+++ condiconaldoble/ejercicio1.py
@@ -0,0 +1,19 @@
+import os
+#declarar variables
+
+consultora,numero_de_productos,precio_de_producto="",0.0,0.0
+verificador= False
+
+#input
+consultora=os.sys.argv[1]
+numero_de_productos=int(os.sys.argv[2])
+precio_de_producto=float(os.sys.argv[3])
+
+#processing
+costo_producto=(numero_de_productos*precio_de_producto)
+verificador=(costo_producto>70)
+
+if(costo_producto>70):
+    print("UD es un comprador excesivo")
+else:
+    print("UD es un comprador moderado")
