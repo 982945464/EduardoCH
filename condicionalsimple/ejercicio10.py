cb774381bd51c526a579379141caa11be81c1051
982945464 <57601238+982945464@users.noreply.github.com>
2019-11-17 01:22:55 -0500

    Se creo el 10mo ejecicio de condicional simple

---
+++ condicionalsimple/ejercicio10.py
@@ -0,0 +1,15 @@
+import os
+#declarar variables
+examen1,examen2,examen3=0.0,0.0,0.0
+promedio=0.0
+
+#input
+examen1=int(os.sys.argv[1])
+examen2=int(os.sys.argv[2])
+examen3=int(os.sys.argv[2])
+
+#processing
+promedio_final=round(examen1+examen2+examen3)/3
+
+if(promedio_final>14):
+    print("ud aprovo el curso")
