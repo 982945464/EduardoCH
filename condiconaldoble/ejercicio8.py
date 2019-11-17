2abb517ea2070852b4e0a9decbf5f57ae9f2cf7c
982945464 <57601238+982945464@users.noreply.github.com>
2019-11-17 01:47:42 -0500

    Se creo el 8vo ejecicio de condicional doble

---
+++ condiconaldoble/ejercicio8.py
@@ -0,0 +1,18 @@
+import os
+#declarar variables
+cliente,numeros_de_zapatillas,precio_de_zapatilla="",0.0,0.0
+verficador= False
+
+#input
+cliente=os.sys.argv[1]
+numeros_de_zapatillas=int(os.sys.argv[2])
+precio_de_zapatilla=float(os.sys.argv[3])
+
+#processing
+costo_zapatilla=(numeros_de_zapatillas*precio_de_zapatilla)
+verificador=(costo_zapatilla>400)
+
+if(costo_zapatilla>400):
+    print("oferta: ud ha ganado una mochila")
+else:
+    print("oferta: ud ha ganado una cartuchera")
